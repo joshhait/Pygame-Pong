555b513ba1c034339e331523c223e9d0d016d894
joshhait <44282219+joshhait@users.noreply.github.com>
2019-01-23 16:45:04 -0800

    created ball class, rather than global vars

--- pong.py
+++ pong.py
@@ -8,13 +8,15 @@ win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Pong")
 
-# ball conditions
-ball_x = 256
-ball_y = 128
-ball_rad = 5
-ball_x_vel = 10
-ball_y_vel = 10
+class Ball:
+    def __init__(self, xpos, ypos, rad, xvel, yvel):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.rad = rad
+        self.xvel = xvel
+        self.yvel = yvel
 
+ball = Ball(256, 128, 5, 10, 10)
 run = True
 
 while run:
@@ -24,21 +26,20 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    
-    if(ball_x <= 0):
-        ball_x_vel = -ball_x_vel
-    if(ball_x >= 512):
-        ball_x_vel = -ball_x_vel
-    if(ball_y <= 0):
-        ball_y_vel = -ball_y_vel
-    if(ball_y >= 256):
-        ball_y_vel = -ball_y_vel
-    ball_x += ball_x_vel
-    ball_y += ball_y_vel
+    if(ball.xpos <= 0):
+        ball.xvel = -ball.xvel
+    if(ball.xpos >= 512):
+        ball.xvel = -ball.xvel
+    if(ball.ypos <= 0):
+        ball.yvel = -ball.yvel
+    if(ball.ypos >= 256):
+        ball.yvel = -ball.yvel
+    ball.xpos += ball.xvel
+    ball.ypos += ball.yvel
 
 
     win.fill((55,55,55))
-    pygame.draw.circle(win, (200, 200, 200), (ball_x, ball_y), ball_rad, 0)
+    pygame.draw.circle(win, (200, 200, 200), (ball.xpos, ball.ypos), ball.rad, 0)
     pygame.display.update()
 
 pygame.quit()
